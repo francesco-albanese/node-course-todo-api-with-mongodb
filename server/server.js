8cdaafc9b378c746c22787c02be657434e2f7991
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-10-29 12:29:08 +0100

    Added new route GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -33,6 +33,22 @@ app.get('/todos', (req, res) => {
 })
 
 // GET /todos/1344aasd78s00fsd
+
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const idIsValid = ObjectID.isValid(id);
+    if (!idIsValid) {
+        return res.status(404).send()
+     }
+    Todo.findById(id).then(todo => {
+        if (!todo) {
+            return res.status(404).send()
+        }
+        res.status(200).send({todo})
+    }).catch(err => {
+        res.status(400).send();
+    });
+})
 
 app.listen('3000', () => {
     console.log(`Started on port ${3000}`)
