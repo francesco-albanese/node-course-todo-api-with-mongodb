82e01b443d067a13fff329435b4d3796bff6faf2
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-11-12 18:06:52 +0100

    Setup app for heroku, added mongolab

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const { User } = require('./models/user');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 //with this we can now send JSON to the server
 app.use(bodyParser.json());
 
@@ -50,8 +52,8 @@ app.get('/todos/:id', (req, res) => {
     });
 })
 
-app.listen('3000', () => {
-    console.log(`Started on port ${3000}`)
+app.listen(port, () => {
+    console.log(`Started on port ${port}`)
 });
 
 module.exports = { app };
