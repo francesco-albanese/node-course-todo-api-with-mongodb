8d1069c4c3a78a5a27593832d79a316711ae824e
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-12-14 12:27:44 +0100

    added tests for POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -68,7 +68,7 @@ UserSchema.statics.findByToken = function(token) {
     try {
         decoded = jwt.verify(token, 'abc123')
     } catch(e) {
-        return Promise.reject()
+        return Promise.reject(e)
     }
 
     return User.findOne({
