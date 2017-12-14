45383fa470a4168497d5c571a16f6869b8608f8f
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-12-14 13:17:50 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -283,24 +283,42 @@ describe('/POST /users/login', () => {
 
     it('should reject invalid login', done => {
         request(app)
-        .post('/users/login')
-        .send({
-            email: users[1].email,
-            password: users[1].password + 'blah'
-        })
-        .expect(400)
-        .expect((res) => {
-            expect(res.headers['x-auth']).toNotExist()
-        })
-        .end((err, res) => {
-            if (err) {
-                done(err)
-            }
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + 'blah'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist()
+            })
+            .end((err, res) => {
+                if (err) {
+                    done(err)
+                }
 
-            User.findById(users[1]._id).then(user => {
-                expect(user.tokens.length).toBe(0)
-                done()
-            }).catch(e => done(e))
-        })
+                User.findById(users[1]._id).then(user => {
+                    expect(user.tokens.length).toBe(0)
+                    done()
+                }).catch(e => done(e))
+            })
+    })
+})
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', done => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    done(err)
+                }
+                User.findById(users[0]._id).then(user => {
+                    expect(user.tokens.length).toBe(0)
+                    done()
+                }).catch(e => done(e))
+            })
     })
 })
