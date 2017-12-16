e8145686646dd85eeb8a04eb59e67ca0e8c9bdaf
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2017-12-16 16:02:32 +0100

    Moved config and envConfig inside the if statement because vars not needed on production

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 const env = process.env.NODE_ENV || "development"
-const config = require('./config.json')
-const envConfig = config[env]
 
 if (env === "development" || env === "test") {
+    const config = require('./config.json')
+    const envConfig = config[env]
     Object.keys(envConfig).forEach(key => {
         process.env[key] = config[env][key]
     })
